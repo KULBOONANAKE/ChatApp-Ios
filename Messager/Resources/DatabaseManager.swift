ae4f1af01a184c29d581f62b232a0ddacfd5feee
WhatWR <kul.b@ku.th>
2023-02-20 00:37:39 +0700

    handle logout and logout alert

--- Messager/Resources/DatabaseManager.swift
+++ Messager/Resources/DatabaseManager.swift
@@ -22,7 +22,10 @@ extension DatabaseManager {
     
     public func userExists(with email:String, completion: @escaping ((Bool) -> Void) ){
         
-        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+        var selfEmail = email.replacingOccurrences(of: ".", with: "-")
+        selfEmail = selfEmail.replacingOccurrences(of: "@", with: "-")
+        
+        database.child(selfEmail).observeSingleEvent(of: .value, with: { snapshot in
             guard snapshot.value as? String != nil else {
                 completion(false)
                 return
@@ -34,7 +37,7 @@ extension DatabaseManager {
     
     /// inserts new user to database
     public func insertUser(with user: ChatAppUser){
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName,
         ])
@@ -46,5 +49,11 @@ struct ChatAppUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
     //    let profilePictureUrl: String
 }
